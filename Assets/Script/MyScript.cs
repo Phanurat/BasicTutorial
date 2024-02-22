c4248a2b77449ab0a808d5f4cf00300e12fa614e
Phanurat <paunavt5@gmail.com>
2024-02-22 17:43:43 +0700

    Object Input from Keyboard

--- Assets/Script/MyScript.cs
+++ Assets/Script/MyScript.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class MyScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 10.0f; //speed วัตถุ
+    private Rigidbody rb;
+    public Vector3 movement;
+
     void Start()
     {
-        Debug.Log("Starting Running");
+        //Debug.Log("Starting Running");
+        rb = GetComponent<Rigidbody>();
     }
 
-    private void Awake()
+    /*private void Awake()
     {
         Debug.Log("Awake Running");
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log("Update Running");
+
+        //รับค่าจาก Keyboard
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Jump");
+        float z = Input.GetAxis("Vertical");
+
+        movement = new Vector3(x, y, z);
     }
 
-    private void FixedUpdate(){
+    /*private void FixedUpdate(){
         Debug.Log("FixedUpdate Running");
-    }
+    }*/
 }
